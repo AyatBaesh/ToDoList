89e22fece71fa46c53dd63fd01b6d4eee7dfbef3
Ayat <ayatbaeshev@gmail.com>
2023-06-21 21:04:40 +0200

    working on functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-const createProject = document.querySelector('#createPRoject');
-
+const createProject = document.querySelector('#createProject');
+const addFormButtons = document.querySelectorAll('.addFormBtn');
+const addForm = document.querySelector('#addForm');
+const projects = document.querySelector('#projects');
 createProject.addEventListener('click',() => {
-    
+    addForm.style.display = "block";
 })
+
+addFormButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        if(event.target.id === 'add'){
+            console.log('add clicked');
+            addProject();
+        }
+        addForm.style.display = 'none';
+    });
+});
+
+function addProject() {
+    const projectNameInput = document.querySelector('#projectName');
+    const projectName = projectNameInput.value;
+    const projectDiv = document.createElement('div');
+    projectDiv.innerText = projectName;
+    projectDiv.classList.add = 'project';
+    projects.insertBefore(projectDiv, projects.firstChild);
+    projectNameInput.value = '';
+}

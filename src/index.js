12e2089070b1442d3cc332924c2ab0464499578e
buyshow <ayatbaeshev@gmail.com>
2023-06-25 17:36:31 +0200

    work in progres

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const createProject = document.querySelector('#createProject');
 const addFormButtons = document.querySelectorAll('.addFormBtn');
 const addForm = document.querySelector('#addForm');
 const projectsDiv = document.querySelector('#projects');
+const projectsContent = document.querySelector('#projectsContent');
 createProject.addEventListener('click',() => {
     addForm.style.display = "block";
 })
@@ -11,25 +12,68 @@ addFormButtons.forEach((button) => {
         event.preventDefault();
         if(event.target.id === 'add'){
             console.log('add clicked');
-            addProject();
+                const projectName = document.querySelector('#projectName').value;
+                const project = new Project(projectName, []); 
+                console.log('Project created:', project);
+                addProject(project);
+                document.querySelector('#projectName').value = '';
         }
         addForm.style.display = 'none';
     });
 });
 
-function addProject() {
-    const projectNameInput = document.querySelector('#projectName');
-    const projectName = projectNameInput.value;
-    const projectDiv = document.createElement('div');
-    projectDiv.innerText = projectName;
-    projectDiv.classList.add('project');
-    projectsDiv.insertBefore(projectDiv, projectsDiv.firstChild);
-    projectNameInput.value = '';
-    projectContent(projectDiv);
-}
-function projectContent(projectDiv){
+function addProject(project){
+    //creating project in sidebar
+    const newProject = createSideProject(project);
+    //adding project to the content div
+    const content = document.createElement('div');
+    content.classList.add('projectContent');
+    const projectHeader = document.createElement('h2');
+    projectHeader.innerText = project.name;
+    content.appendChild(projectHeader);
+    
+    const toDosContainer = document.createElement('div');
+    toDosContainer.classList.add('toDos');
 
+    
+    toDosContainer.appendChild(TodoInput);
+    const toDoAdd = document.createElement('button');
+    toDoAdd.type = 'button';
+    toDoAdd.name = 'addButton';
+    toDoAdd.id = 'addButton';
+    toDoAdd.textContent = 'Add';
+    toDoAdd.classList.add('btn');
+    toDosContainer.appendChild(toDoAdd);
+ 
+    toDoAdd.addEventListener('click', (event) => {
+        const toDo = createTodo(TodoInput.value);
+        const toDoContainer = document.createElement('div');
+        toDoContainer.innerText = toDo.name;
+        toDosContainer.insertBefore(toDoContainer, toDosContainer.firstChild);
+    });
+    content.appendChild(toDosContainer);
+    projectsContent.appendChild(content);
 }
+function createSideProject(project){
+    const newProject = document.createElement('div');
+    newProject.classList.add('project');
+    newProject.innerText = project.name; 
+    projectsDiv.appendChild(newProject);
+    return newProject;
+}
+function createTodoInput(){
+    const TodoInput = document.createElement('input');
+    TodoInput.type = 'text';
+    TodoInput.name = 'todo';
+    TodoInput.id = 'todoInput';
+    return TodoInput;
+}
+function createTodo(name){
+    const toDo = new ToDo(name);
+
+    return toDo;
+}
+
 
 class Project {
     constructor(name, tasks){
@@ -44,4 +88,18 @@ class Project {
             return task.name != taskToRemove.name;
         });
     }
+}
+
+class ToDo{
+    constructor(name){
+        this.name = name;
+        // this.date = dueDate;
+    }
+    setName(name){
+        this.name = name;
+    }
+    getName(){
+        return this.name;
+    }
+
 }

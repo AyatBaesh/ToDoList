3d4f1e8edf446f93b869ee2f069b80f4084fd603
buyshow <ayatbaeshev@gmail.com>
2023-06-25 17:41:36 +0200

    minor update

--- src/index.js
+++ src/index.js
@@ -22,57 +22,60 @@ addFormButtons.forEach((button) => {
     });
 });
 
-function addProject(project){
-    //creating project in sidebar
-    const newProject = createSideProject(project);
-    //adding project to the content div
-    const content = document.createElement('div');
-    content.classList.add('projectContent');
-    const projectHeader = document.createElement('h2');
-    projectHeader.innerText = project.name;
-    content.appendChild(projectHeader);
+const DOMstuff = (() => {
+    function addProject(project){
+        //creating project in sidebar
+        const newProject = createSideProject(project);
+        //adding project to the content div
+        const content = document.createElement('div');
+        content.classList.add('projectContent');
+        const projectHeader = document.createElement('h2');
+        projectHeader.innerText = project.name;
+        content.appendChild(projectHeader);
+        
+        const toDosContainer = document.createElement('div');
+        toDosContainer.classList.add('toDos');
     
-    const toDosContainer = document.createElement('div');
-    toDosContainer.classList.add('toDos');
-
+        
+        toDosContainer.appendChild(TodoInput);
+        const toDoAdd = document.createElement('button');
+        toDoAdd.type = 'button';
+        toDoAdd.name = 'addButton';
+        toDoAdd.id = 'addButton';
+        toDoAdd.textContent = 'Add';
+        toDoAdd.classList.add('btn');
+        toDosContainer.appendChild(toDoAdd);
+     
+        toDoAdd.addEventListener('click', (event) => {
+            const toDo = createTodo(TodoInput.value);
+            const toDoContainer = document.createElement('div');
+            toDoContainer.innerText = toDo.name;
+            toDosContainer.insertBefore(toDoContainer, toDosContainer.firstChild);
+        });
+        content.appendChild(toDosContainer);
+        projectsContent.appendChild(content);
+    }
+    function createSideProject(project){
+        const newProject = document.createElement('div');
+        newProject.classList.add('project');
+        newProject.innerText = project.name; 
+        projectsDiv.appendChild(newProject);
+        return newProject;
+    }
+    function createTodoInput(){
+        const TodoInput = document.createElement('input');
+        TodoInput.type = 'text';
+        TodoInput.name = 'todo';
+        TodoInput.id = 'todoInput';
+        return TodoInput;
+    }
+    function createTodo(name){
+        const toDo = new ToDo(name);
     
-    toDosContainer.appendChild(TodoInput);
-    const toDoAdd = document.createElement('button');
-    toDoAdd.type = 'button';
-    toDoAdd.name = 'addButton';
-    toDoAdd.id = 'addButton';
-    toDoAdd.textContent = 'Add';
-    toDoAdd.classList.add('btn');
-    toDosContainer.appendChild(toDoAdd);
- 
-    toDoAdd.addEventListener('click', (event) => {
-        const toDo = createTodo(TodoInput.value);
-        const toDoContainer = document.createElement('div');
-        toDoContainer.innerText = toDo.name;
-        toDosContainer.insertBefore(toDoContainer, toDosContainer.firstChild);
-    });
-    content.appendChild(toDosContainer);
-    projectsContent.appendChild(content);
-}
-function createSideProject(project){
-    const newProject = document.createElement('div');
-    newProject.classList.add('project');
-    newProject.innerText = project.name; 
-    projectsDiv.appendChild(newProject);
-    return newProject;
-}
-function createTodoInput(){
-    const TodoInput = document.createElement('input');
-    TodoInput.type = 'text';
-    TodoInput.name = 'todo';
-    TodoInput.id = 'todoInput';
-    return TodoInput;
-}
-function createTodo(name){
-    const toDo = new ToDo(name);
-
-    return toDo;
-}
+        return toDo;
+    }
+    
+})
 
 
 class Project {

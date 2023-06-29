0cc3ac8d104657d3af313b4f1183bffeba3b2fc0
Ayat <ayatbaeshev@gmail.com>
2023-06-29 12:37:29 +0200

    DOMModule created, new methods addet to the ToDoDTO class

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
 const addButton = document.getElementById('addButton');
 const toDoInput = document.getElementById('toDoInput');
 const toDoDiv = document.querySelector('.toDos');
+const todos = [];
 addButton.addEventListener('click', (event) => {
-    createToDo(toDoInput.value);
-    saveData();
+    DOMModule.createToDo(toDoInput.value);
+    // saveData();
 })
+const DOMModule = (() =>{
+    function createToDo(name){
+        const toDo = new ToDoDTO(name, false);
+        todos.push(toDo);
+        const toDoContainer = document.createElement('div');
+        toDoContainer.classList.add('toDo');
+        toDoContainer.innerText = toDo.getName();
+        toDoDiv.appendChild(toDoContainer);
+    
+    }
+    function createToDoButtons(toDoContainer){
+        const changeButton = document.createElement('button');
+        changeButton.id = 'changeButton';
+        changeButton.type = 'button';
+        changeButton.classList.add('toDoButton');
+        
+        const statusButton = document.createElement('button');
+        statusButton.id = 'statusButton';
+        changeButton.type = 'button';
+        changeButton.classList.add('toDoButton');
 
-function createToDo(name){
-    const toDo = new ToDoDTO(name);
-    const toDoContainer = document.createElement('div');
-    toDoContainer.classList.add('toDo');
-    toDoContainer.innerText = toDo.getName();
-    toDoDiv.appendChild(toDoContainer);
-}
+        const deleteButton = document.createElement('button');
+        deleteButton.id = 'deleteButton';
+        deleteButton.tpye = 'button';
+        deleteButton.classList.add('toDoButton');
+
+    }
+    return {createToDo}
+})();
+
+
+
 class ToDoDTO{
-    constructor(name){
+    constructor(name, status){
+        this.status = status;
         this.name = name;
     }
     setName(name){
@@ -23,4 +49,18 @@ class ToDoDTO{
     getName(){
         return this.name;
     }
+    setStatus(status){
+        this.status = status;
+    }
+    getStatus(){
+        return this.status;
+    }
+
+}
+
+class ToDoList{
+    constructor(todos){
+        this.todos = todos;
+    }
+    
 }

2da4edaef6ae0dcf393c1e548800b9847362e608
Ayat <ayatbaeshev@gmail.com>
2023-08-14 20:21:21 +0200

    buttons functionality added, icons added, working on sending data to backend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const addButton = document.getElementById('addButton');
 const toDoInput = document.getElementById('toDoInput');
 const toDoDiv = document.querySelector('.toDos');
-const todos = [];
+let todos = [];
 // let exists = false;
 
 function checkName(name, todos){
@@ -14,62 +14,122 @@ function checkName(name, todos){
 
 }
 
+// async function getData(){
+//     let dataArr = await fetch(/*fetch data */);
+//     todos = dataArr.json();
+//     renderToDos(todos);
+// }
 addButton.addEventListener('click', (event) => {
     if(toDoInput.value){
         if(checkName(toDoInput.value, todos)){
             alert('You already have this task!');
             return;
+        }else if(toDoInput.value.length > 58){
+            alert('Oops, Looks like your ToDo name is too big, consider renaming it or breaking into smaller tasks');
         }
         DOMModule.createToDo(toDoInput.value);
-        console.log(`todos: ${todos}`)
-    // saveData();
+        toDoInput.value = '';
+
+
+        console.log(`todos: ${todos}`);
     }
 })
+function renderToDos(todos){
+    DOMModule.clear();
+    todos.forEach(todo => {
+        DOMModule.createToDo(todo.name);
+    });
+}
 const DOMModule = (() =>{
-    function createToDo(name){
-        const toDo = new ToDoDTO(name, false);
-        todos.push(toDo);
-        const toDoContainer = document.createElement('div');
-        toDoContainer.classList.add('toDo');
-        toDoContainer.innerText = toDo.getName();
-        toDoDiv.appendChild(toDoContainer);
-        createToDoButtons(toDoContainer);
     
+    function clear(){
+        while(toDoDiv.firstChild){
+            toDoDiv.removeChild(toDoDiv.firstChild);
+        }
+        todos = [];
     }
-    function createToDoButtons(toDoContainer){
-        const changeButton = document.createElement('button');
-        changeButton.id = 'changeButton';
-        changeButton.type = 'button';
-        changeButton.classList.add('toDoButton');
-        changeButton.innerText = 'change';
+
+    function createToDo(name){
+        const toDo = new ToDoDTO(name, false, (todos.length + 1));
+        todos.push(toDo);
+        let jsonToDo = JSON.stringify(toDo);
+        //send toDo to the server
+        createToDoContainer(toDo);    
+
+    }
+    
+    function createToDoContainer(toDo){
+
+            const toDoContainer = document.createElement('div');
+            toDoContainer.classList.add('toDo');
+            toDoContainer.innerText = toDo.getName();
+            toDoDiv.appendChild(toDoContainer);
+            createToDoButtons(toDoContainer, toDo);
+
+    }
+    function createToDoButtons(toDoContainer, currentToDo){
+
+        const buttonsContainer = document.createElement('div');
+        buttonsContainer.classList.add('buttons-container');
         
-        const statusButton = document.createElement('button');
+        const statusButton = document.createElement('i');
         statusButton.id = 'statusButton';
-        statusButton.type = 'button';
         statusButton.classList.add('toDoButton');
-        statusButton.innerText = 'status';
+        statusButton.classList.add('fa-solid', 'fa-check');
+        statusButton.addEventListener('click', () => updateToDo(currentToDo, toDoContainer));
 
-
-        const deleteButton = document.createElement('button');
+        const deleteButton = document.createElement('i');
         deleteButton.id = 'deleteButton';
-        deleteButton.tpye = 'button';
         deleteButton.classList.add('toDoButton');
-        deleteButton.innerText = 'delete';
+        deleteButton.classList.add('fa-solid', 'fa-xmark');
+        deleteButton.addEventListener('click', () => deleteToDo(todos, currentToDo));
 
-        toDoContainer.appendChild(changeButton);
-        toDoContainer.appendChild(statusButton);
-        toDoContainer.appendChild(deleteButton);
-
+        buttonsContainer.appendChild(statusButton);
+        buttonsContainer.appendChild(deleteButton);
+        
+        toDoContainer.appendChild(buttonsContainer);
+        
     }
-    return {createToDo}
+
+    function deleteToDo(todos, deletedToDo){
+        //send deleted todo
+        let jsonDeletedToDo = JSON.stringify(deletedToDo);
+        todos = todos.filter(todo => todo.name != deletedToDo.name);
+        renderToDos(todos);
+        return;
+    }
+    function updateToDo(updatedToDo, toDoContainer){
+        let jsonUpdatedToDo = JSON.stringify(updatedToDo);
+
+        if(toDoContainer.classList.contains('done')){
+            toDoContainer.classList.replace('done', 'undone');
+        }else if(toDoContainer.classList.contains('undone')){
+            toDoContainer.classList.replace('undone', 'done');
+        }else if(updatedToDo.getStatus()){
+
+            toDoContainer.classList.add('done');
+        }else if(!updatedToDo.getStatus()){
+            toDoContainer.classList.add('done');
+        }
+        updatedToDo.changeStatus();
+        return;
+
+
+        //send udpated todo
+    }
+
+
+
+    return {createToDo, clear}
 })();
 
-
+// console.log(`deleteButton ${deleteButton}`);
 
 class ToDoDTO{
-    constructor(name, status){
+    constructor(name, status, index){
         this.status = status;
         this.name = name;
+        this.index = index;
     }
     setName(name){
         this.name = name;
